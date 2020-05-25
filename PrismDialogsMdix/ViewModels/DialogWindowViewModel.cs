8ba37e925cc91a06631202f9474ef2aac817b830
Ray Hayes <ray.hayes@gmail.com>
2020-05-25 12:03:04 +0100

    Adding basics of a dialog from example Prism release notes
    Also from https://www.youtube.com/watch?v=fKtJPDygu1M&t=7488s

---
+++ PrismDialogsMdix/ViewModels/DialogWindowViewModel.cs
@@ -0,0 +1,26 @@
+﻿namespace PrismDialogsMdix.ViewModels
+{
+    using System.Diagnostics;
+    using System.Windows.Input;
+
+    using Prism.Commands;
+    using Prism.Mvvm;
+
+    public class DialogWindowViewModel : BindableBase
+    {
+        private string message;
+
+        public DialogWindowViewModel()
+        {
+            CloseDialogCommand = new DelegateCommand(() => Trace.WriteLine("To do close"));
+        }
+
+        public string Message
+        {
+            get => message;
+            set => SetProperty(ref message, value);
+        }
+
+        public ICommand CloseDialogCommand { get; }
+    }
+}
---
+++ PrismDialogsMdix/Views/DialogWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace PrismDialogsMdix.Views
+{
+    using System.Windows.Controls;
+
+    /// <summary>
+    /// Interaction logic for DialogWindow.xaml
+    /// </summary>
+    public partial class DialogWindow : UserControl
+    {
+        public DialogWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
